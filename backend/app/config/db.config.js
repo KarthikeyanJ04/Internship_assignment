be1b8562892066ea9cd5617ff816b4ed20ba658b
KarthikeyanJ04 <karthikeyanjayagandhan04@gmail.com>
2025-11-27 15:34:21 +0000

    Made Dockerfile and docker-compose for backend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 module.exports = {
-  url: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/dd_db'
+  url: process.env.MONGO_URI || 'mongodb://mongo:27017/dd_db'
 };
